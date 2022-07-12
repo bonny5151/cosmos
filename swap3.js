4a274affae26cc018479c33a9922e3cf0e988050
root <z11a11@protonmail.com>
2022-07-12 15:13:53 +0000

    a

--- swap3.js
+++ swap3.js
@@ -40,9 +40,11 @@ v1 = await osmclient.signAndBroadcast(w1.address, [msg], fee1,'')
 //v = await cosmology.signAndBroadcast({client: osmclient, chainId: 'osmosis-1', address: w1.address, msg: msg, fee: fee1, memo: '' });
 vv = await osmclient.sendTokens(w1.address, w11.address, a.coins(1,ustc),fee1)
 atom1 = "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
-v2 = await osmclient.sendIbcTokens(w1.address, w.address, a.coin(1,atom1),"transfer", "channel-0", "", Math.floor(Date.now() / >
+
+v2 = await osmclient.sendIbcTokens(w1.address, w.address, a.coin(1,atom1),"transfer", "channel-0", "", Math.floor(Date.now() / 1000) + 60, fee1)
 c1 = 'cro1cu3pfakqz7mu5fy0rjr9a8ck8uxpcgl7yt9yjg'
 cro = "ibc/E6931F78057F7CC5DA0FD6CEF82FF39373A6E0452BF1FD76910B93292CF356C1"
-v2 = await osmclient2.sendIbcTokens(w1.address, c1, a.coin(1,cro),"transfer", "channel-5", "", Math.floor(Date.now() / 1000) + >
-c1
+v2 = await osmclient.sendIbcTokens(w1.address, c1, a.coin(1,cro),"transfer", "channel-5", "", Math.floor(Date.now() / 1000) + 60, fee1)
+
+
 
