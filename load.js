12b0de15c89a62eb829c6cfe489de418d4a33d9f
root <z11a11@protonmail.com>
2022-07-15 21:05:08 +0000

    a

--- load.js
+++ load.js
@@ -2,17 +2,21 @@ lib = { s : require("@cosmjs/proto-signing"),
         g : require("@cosmjs/stargate"),
        e :  require("@cosmjs/encoding"),
       a : require("@cosmjs/amino"),
-     osmojs : require("osmojs")
+     osmojs : require("osmojs"),
+    cosmology : require("@cosmology/core")
+
  }
 
-function getfee(gaslimit=100000, gasprice="0.025", token="uosmo") {
+function getfee(gaslimit=100000, gasprice="0.025", token="uatom") {
+   gaslimit = gaslimit ? gaslimit : 100000;
+   gasprice = gasprice ? gasprice : "0.025"
+   token = token ? token : "uatom"
    return lib.g.calculateFee(gaslimit, lib.g.GasPrice.fromString(gasprice + token))
 } 
 function coins(amount, token) {
   return lib.a.coins(amount, token)
 }
 async function walletfromkey(key, chain="cosmos") {
-var key;
 var w;
 if(key.indexOf(" ") >=0) {
  if(chain != 'cosmos') { chain = {prefix: chain}};
@@ -28,28 +32,61 @@ key  = lib.e.fromBase64(key)
 return w
 }
 
-async function connectrpc(rpcurl, wallet)
-{
-    if(wallet.address.toLowerCase().startsWith("osmo")) {
-      return lib.osmojs.getSigningOsmosisClient({rpcEndpoint:rpcurl, signer: wallet})
-   }
-   return lib.g.SigningStargateClient.connectWithSigner(rpcurl, wallet)
-}
-key = "G9KxuXlDbM7wj0YRu8ahAOIuVbJjIVBcweNSjPWFfwg="
-
-w =await walletfromkey(key)
-
-w1 =await walletfromkey(key, "osmo")
-
 osmorpc = "https://rpc-osmosis.whispernode.com"
 cosmrpc = "https://rpc-cosmoshub.ecostake.com"
-osmclient = await connectrpc(osmorpc, w1)
-cosmclient = await connectrpc(cosmrpc, w)
 
-await cosmclient.getBalance(cosmclient.signer.address, "uatom")
-await osmclient.getBalance(osmclient.signer.address, "uosmo")
-key2 = "DRd/yMuKF/YiTsa+EUFm6i19kvfHuZsqxgYKSQvC/cs="
-w11 = await walletfromkey(key2, "osmo")
-w01 = await walletfromkey(key2)
-fee = getfee(100000,.025,"uatom")
+async function connectrpc(wallet, rpcurl)
+{
+   var addr = wallet.address.toLowerCase();
+  
+    if(addr.startsWith("osmo")) {
+      if(!rpcurl) {rpcurl = osmorpc;}
+      return lib.osmojs.getSigningOsmosisClient({rpcEndpoint:rpcurl, signer: wallet})
+   }
+   if(!rpcurl) {rpcurl = cosmrpc;}
+   return lib.g.SigningStargateClient.connectWithSigner(rpcurl, wallet)
+}
+function getfeetoken(connection) {
+  var address = connection.address ? connection.address: connection.signer.address
+  address = address.toLowerCase()
+  return address.startsWith("osmo") ? "uosmo" : "uatom"
+}
+async function sendibctokens(connection, toaddress, tokens, destchannel)
+{
+ var feetoken = getfeetoken(connection)
+ tokens = Array.isArray(tokens) ? tokens[0] : tokens
+ var fee1 = getfee(250000, "0.025", feetoken)
+ return connection.sendIbcTokens(connection.signer.address, toaddress, tokens, "transfer", destchannel, "", Math.floor(Date.now() / 1000) + 60, fee1)
 
+}
+
+async function sendtokens(connection, toaddress, tokens)
+{
+  var feetoken = getfeetoken(connection)
+  var fee1 = getfee(100000, "0.025",feetoken)
+  tokens = Array.isArray(tokens) ? tokens : [tokens]
+  return connection.sendTokens(connection.signer.address, toaddress, tokens, fee1)
+}
+
+async function swaposmosis(connection, route,inputtokens, minoutputamount)
+{
+   var addr = connection.signer.address      
+   var fee1 = getfee(250000, "0.025", getfeetoken(connection))
+   inputtokens = Array.isArray(inputtokens) ?  inputtokens[0] : inputtokens
+   var msg = lib.cosmology.messages.swapExactAmountIn({sender: addr, routes: route, tokenIn: inputtokens , tokenOutMinAmount: minoutputamount})
+   return connection.signAndBroadcast(addr, [msg], fee1)
+}
+
+ibctokens= {
+   atom : "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
+   ustc : "ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC"
+}
+
+buyustcwithosmoroute = [{poolId: 560, tokenOutDenom: ibctokens.ustc}]
+
+var key1 = "G9KxuXlDbM7wj0YRu8ahAOIuVbJjIVBcweNSjPWFfwg="
+
+//await cosmclient.getBalance(cosmclient.signer.address, "uatom")
+//await osmclient.getBalance(osmclient.signer.address, "uosmo")
+var key2 = "DRd/yMuKF/YiTsa+EUFm6i19kvfHuZsqxgYKSQvC/cs="
+fee = getfee()
